531c835b92f4f58f9e468ae34ea54fd9284d710b
Millad Ayoubi <Milladayoubi@gmail.com>
2021-02-05 13:20:31 -0500

    File Setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+const path = require('path')
 const express = require('express')
+
+
+
+const app = express()
+
+
+const port = process.env.port || 3000
+const publicPath = path.join(__dirname, '../public')
+
+
+app.use(express.static(publicPath))
+
+
+app.listen(port, () => {
+    console.log('Server Is Up On ' + port)
+})
