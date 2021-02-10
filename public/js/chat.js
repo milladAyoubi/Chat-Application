405de821eeeb68fde71a58a0e9b3ba9a2204b602
Millad Ayoubi <Milladayoubi@gmail.com>
2021-02-10 14:20:54 -0500

    Refactor

---
+++ public/js/chat.js
@@ -0,0 +1,23 @@
+//Client Logs countUpdated Event From Server
+
+const socket = io()
+
+socket.on('countUpdated', (count) => {
+    console.log('Count has been updated', count)
+    display.innerHTML = 'Button Has Been Click ' + count
+})
+
+
+socket.on('welcomeMessage', (message) => {
+    const title = document.querySelector('#title')
+    title.innerHTML = message
+})
+const display = document.querySelector('#countDisplay')
+
+//Button That Initiates Serverside and Client Side Exchange
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+        //Runs increment event on serverSide
+    socket.emit('increment')
+
+})
--- src/index.js
+++ src/index.js
@@ -3,25 +3,34 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 
+
+
+//Connecting Server
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+
+//Joining static files (HTML,CSS,Client Side JS) from directory
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 let count = 0
+const message = "Welcome To Unova Chat!"
 
+//Runs On Socket.io Connection to server
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
         //Sending event to client 
-    socket.emit('countUpdated', count)
+    socket.emit('welcomeMessage', message)
         //Reciveing Increment from client side
     socket.on('increment', () => {
         count++
         socket.emit('countUpdated', count)
+
+
     })
 })
 
